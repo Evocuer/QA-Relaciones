9a6792d1da7a80f50292bce93112e0d719907760
Evocuer <ing.cuervodiaz@gmail.com>
2023-08-15 19:40:17 -0500

    Entidades y diagrama UML generados

---
+++ RECEAEJ1/src/main/java/Entidad/Perro.java
@@ -0,0 +1,51 @@
+package Entidad;
+
+public class Perro {
+
+    private String nombre;
+    private String raza;
+    private Integer edad;
+    private String tamanho;
+
+    public Perro() {
+    }
+
+    public Perro(String nombre, String raza, Integer edad, String tamanho) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+        this.tamanho = tamanho;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+}
---
+++ RECEAEJ1/src/main/java/Entidad/Persona.java
@@ -0,0 +1,13 @@
+package Entidad;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+    private String id;
+    private Perro perro;
+
+
+
+}
